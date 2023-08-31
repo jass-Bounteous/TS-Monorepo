53589f78cd8e3486a0411d668912a2f5cd0415a3
jass-Bounteous <jayasurya.karthikeyan@bounteous.com>
2023-08-31 12:38:34 +0530

    Changes done

--- jest.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/**/*.test.ts"],
-  forceExit: true,
-};
--- workspaces/package2/types.ts
+++
@@ -1,8 +0,0 @@
-export interface validUserType {
-  name: string;
-  mobile_no: number;
-  email: string;
-  password: string;
-  emp_code: number;
-  dob?: Date;
-}
